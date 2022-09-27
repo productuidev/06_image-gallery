57cd3575141903f8413df5f962c11c11d1f30b32
productuidev <productuidev@gmail.com>
2022-09-28 07:31:07 +0900

    initial case (no image gallery)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <div className="initial--box">
+          <div className="text--center">
+            이미지가 없습니다.<br />이미지를 추가해주세요.
+          </div>
+          <div className="plus--box">
+            +
+          </div>
+        </div>
     </div>
   );
 }
